cad71c381813ce0fbd9748df7cf4ba555105e9bb
Silvan <silvan.belder@wearetriple.com>
2025-02-05 08:55:12 +0100

    refactor(Button): migrate styles to Button.tsx and remove Button.styles.ts

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { Pressable, Text, useWindowDimensions } from "react-native";
-
-import $ from "./Button.styles";
+import { StyleSheet } from "react-native-unistyles";
 
 type Props = {
   children?: string;
   onPress: () => void;
 };
 
+const styles = StyleSheet.create((theme, rt) => ({
+  button: {
+    paddingVertical: theme.spacing(2),
+    paddingHorizontal: theme.spacing(6),
+    width: rt.screen.width, // <-- This doesn't properly update on window resize
+    backgroundColor: rt.screen.width > 500 ? "blue" : "green", // <-- This doesn't properly update on window resize
+  },
+  text: {
+    textAlign: "center",
+    color: "white",
+  },
+}));
+
 const Button: React.FC<Props> = ({ children, onPress }) => {
   const { width } = useWindowDimensions();
 
   return (
     <>
-      <Pressable accessibilityRole="button" onPress={onPress} style={$.button}>
-        <Text accessibilityRole="text" style={$.text}>
-          {children} (width: {$.button.width})
+      <Pressable
+        accessibilityRole="button"
+        onPress={onPress}
+        style={styles.button}
+      >
+        <Text accessibilityRole="text" style={styles.text}>
+          {children} (width: {styles.button.width})
         </Text>
       </Pressable>
 
