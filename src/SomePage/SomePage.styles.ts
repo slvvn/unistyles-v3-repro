60cfc840561a05f13e674940ff67a75474edbaa9
Silvan <silvan.belder@wearetriple.com>
2025-04-28 16:45:15 +0200

    repro

--- src/SomePage/SomePage.styles.ts
+++ src/SomePage/SomePage.styles.ts
@@ -1,10 +1,14 @@
-import { StyleSheet } from "react-native-unistyles";
+import { mq, StyleSheet } from "react-native-unistyles";
 
 const stylesheet = StyleSheet.create(() => ({
   container: {
     padding: 32,
     borderWidth: 4,
     borderColor: "darkblue",
+    backgroundColor: {
+      [mq.only.width(undefined, "xl")]: "red",
+      [mq.only.width("xl")]: "blue",
+    },
   },
   list: {
     backgroundColor: "lightblue",
--- src/SomePage/SomePage.tsx
+++ src/SomePage/SomePage.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { View } from "react-native";
 
 import $ from "./SomePage.styles";
+import { UnistylesRuntime } from "react-native-unistyles";
 
 const SomePage: React.FC = () => {
-  return <View style={$.container}></View>;
+  return <View style={$.container}>{UnistylesRuntime.breakpoint}</View>;
 };
 
 export default SomePage;
