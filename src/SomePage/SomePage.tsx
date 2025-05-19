2c640b82bedaff74bac0945353a5d6bc64e50b84
Silvan <silvan.belder@wearetriple.com>
2025-05-19 15:17:08 +0200

    fix: repro

--- src/SomePage/SomePage.tsx
+++ src/SomePage/SomePage.tsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Hide, mq, UnistylesRuntime } from "react-native-unistyles";
+import { View } from "react-native";
 
 import $ from "./styles";
 
 const SomePage: React.FC = () => {
-  console.log(UnistylesRuntime.breakpoints);
-  return (
-    <View style={$.container}>
-      <Hide mq={mq.only.width("lg")}>
-        <Text>Should be hidden only on large devices</Text>
-      </Hide>
-      <Text>MQ used: {mq.only.width("lg").toString()}</Text>
-    </View>
-  );
+  return <View style={$.container}/>
 };
 
 export default SomePage;
--- src/SomePage/styles.ts
+++ src/SomePage/styles.ts
@@ -1,12 +1,19 @@
-import { mq, StyleSheet } from "react-native-unistyles";
+import { StyleSheet } from "react-native-unistyles";
 
 const stylesheet = StyleSheet.create(() => {
-  console.log("mq used in stylesheet", mq.only.width("lg"));
   return {
     container: {
       padding: 32,
       borderWidth: 4,
-      borderColor: "lightblue",
+      borderColor: {
+        xs: "darkblue",
+        md: "darkgreen",
+      },
+      backgroundColor: {
+        xs: "lightblue",
+        md: "lightgreen",
+        lg: "lightyellow",
+      },
     },
   };
 });
