670ee0b0f045158f6df5350a696fdbac46e81cd1
Silvan <silvan.belder@wearetriple.com>
2025-06-23 10:39:24 +0200

    repro: styles of flatlist not updating on insets change

--- src/SomePage/SomePage.tsx
+++ src/SomePage/SomePage.tsx
@@ -1,33 +1,24 @@
-import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import React from "react";
+import { FlatList, Text, View } from "react-native";
 
 import $ from "./styles";
-import { UnistylesRuntime, useUnistyles } from "react-native-unistyles";
 
 const SomePage: React.FC = () => {
-  const { theme } = useUnistyles();
-  console.log(theme.colors.primary);
-
-  useEffect(() => {
-    setTimeout(() => {
-      UnistylesRuntime.updateTheme("brand", (theme) => ({
-        ...theme,
-        colors: {
-          ...theme.colors,
-          primary: "red",
-        },
-      }));
-    }, 1000);
-  }, []);
-
   return (
     <>
       <View style={$.container}>
         <Text>Updating properly</Text>
       </View>
-      <View style={{ backgroundColor: theme.colors.primary, padding: 32 }}>
-        <Text>Not updating properly</Text>
-      </View>
+
+      <FlatList
+        contentContainerStyle={$.list}
+        data={Array.from({ length: 100 })}
+        renderItem={({ index }) => (
+          <View style={$.item}>
+            <Text>Item {index}</Text>
+          </View>
+        )}
+      />
     </>
   );
 };
--- src/SomePage/styles.ts
+++ src/SomePage/styles.ts
@@ -1,12 +1,54 @@
 import { StyleSheet } from "react-native-unistyles";
 
-const stylesheet = StyleSheet.create((theme) => {
-  return {
-    container: {
-      padding: 32,
-      backgroundColor: theme.colors.primary,
-    },
-  };
-});
+const stylesheet = StyleSheet.create(
+  (theme, { insets, isLandscape, colorScheme }) => {
+    return {
+      container: {
+        padding: 32,
+
+        // This updates well (based on colorScheme dependency)
+        backgroundColor:
+          colorScheme === "dark" ? "rgba(255,0,0,0.5)" : "rgba(0,255,0,0.5)",
+
+        // These update well (based on isLandscape dependency)
+        borderRadius: isLandscape ? 100 : 0,
+
+        // These update well (based on insets dependency)
+        marginLeft: insets.left,
+        marginRight: insets.right,
+        marginBottom: insets.bottom,
+        marginTop: insets.top,
+      },
+
+      list: {
+        marginVertical: 32,
+
+        // This updates well (based on colorScheme dependency)
+        backgroundColor:
+          colorScheme === "dark" ? "rgba(255,0,0,0.5)" : "rgba(0,255,0,0.5)",
+
+        // This does NOT update well (based on isLandscape dependency)
+        borderRadius: isLandscape ? 100 : 0,
+
+        // These does NOT update well (based on insets dependency)
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+        paddingBottom: insets.bottom,
+        paddingTop: insets.top,
+      },
+
+      item: {
+        padding: 32,
+
+        // This updates well (based on colorScheme dependency)
+        backgroundColor:
+          colorScheme === "dark" ? "rgb(255,0,0)" : "rgb(0,255,0)",
+
+        // This updates well (based on isLandscape dependency)
+        borderRadius: isLandscape ? 100 : 0,
+      },
+    };
+  }
+);
 
 export default stylesheet;
